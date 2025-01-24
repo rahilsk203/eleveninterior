63ace15258a00f3ccda57bcd102af64fac656e77
rahilsk203 <rahilsk203@gmail.com>
2025-01-24 21:52:19 +0530

    ract

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-         <Route path="/contact" element={<Contact />} />
+         <Route path="contact" element={<Contact />} />
       </Routes>
     </Router>
 
